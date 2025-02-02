4ec81263ca297edb2822a50345097f2620350a16
Nomor24 <nmorland@unomaha.edu>
2025-02-02 18:23:33 +0000

    Lab is done

--- FutureTime.py
+++ FutureTime.py
@@ -12,18 +12,26 @@ def main():
   currentHour = now.hour
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+
 
   #TODO:
   #Ask user for hours
+  addHours = int(input("Enter the number of hours to add:"))
+
   #Ask user for minutes
+  addMinutes = int(input("enter the number of minutes to add:"))
 
   #Calculate the time after the user-supplied time has passed.
-
-  #Do not use any if statements in calculating the time.
-
-  #Output the future time in standard format "HH:MM"
-
+  totalMinutes = currentMinute + addMinutes
+  newMinute = totalMinutes % 60
+  extraHour = totalMinutes // 60
+  totalHours = currentHour + addHours + extraHour
+  newhour24 = totalHours % 24
+#Do not use any if statements in calculating the time.
+  
+  newhour12 = ((newhour24-1) % 12) + 1
+#Output the future time in standard format "HH:MM"
+  print("The time will be: " + f"{newhour12:02}:{newMinute:02}")
 
 if __name__ == '__main__':
   main()
